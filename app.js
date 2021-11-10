0bb9ada2a1c9b7f160f9a9d396b245e943d46a2e
lavender <id0421942@gmail.com>
2021-11-10 13:04:35 +0800

    feat: routes

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 
+const routes = require('./routes')
+
 const app = express()
 const port = 3000
 
-app.engine('hbs', exphbs({ defaultLayout:'main', extname:'.hbs'}))
-app.set('view engine','hbs')
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
 
 require('./conifg/mongoose')
-app.get('/', (req, res) => {
-    res.render('index')
-})
 
-app.listen(port, () =>{
+app.use(routes)
+
+app.listen(port, () => {
     console.log('App is running on http//:localhost:3000')
 })
