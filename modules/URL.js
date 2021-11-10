7d20399632881856aee253aecb8962ce7d992230
lavender <id0421942@gmail.com>
2021-11-10 15:33:07 +0800

    cohore: heroku init

--- modules/URL.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
-
-const urlSchema = new Schema({
-    shortURL: { type: String, require: true },
-    originaURL: { type: String, require: true },
-})
-
-module.exports = mongoose.model('URL', urlSchema)
--- utils/shortenURL.js
+++ utils/shortenURL.js
@@ -1,10 +1,14 @@
 const BASE_62_CHAR = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 
+const MAX = 61
+
+const MIN = 0
+
 module.exports = (shortenURL_Length) => {
   let result = ""
 
   for (let i = 0; i < shortenURL_Length; i++) {
-    const randomIndex = Math.floor(Math.random() * BASE_62_CHAR.length)
+    const randomIndex = Math.floor(Math.random() * (MAX - MIN + 1) + MIN)
 
     const chooseChar = BASE_62_CHAR[randomIndex]
 
