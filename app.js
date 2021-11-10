8519bb518b074cb4d1e9825f22701895ef2721e9
lavender <id0421942@gmail.com>
2021-11-10 12:04:08 +0800

    feat add hbs and views

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express')
+const exphbs = require('express-handlebars')
 
 const app = express()
 const port = 3000
 
+app.engine('hbs', exphbs({ defaultLayout:'main', extname:'.hbs'}))
+app.set('view engine','hbs')
+
 app.get('/', (req, res) => {
-    res.send('good')
+    res.render('index')
 })
 
 app.listen(port, () =>{
